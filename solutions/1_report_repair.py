be61a5c7da7279b52b7a61e716eee5832ab41883
GaboGomez09 <gomezgabriel09@gmail.com>
2020-12-01 19:20:00 -0600

    First day, part two

--- solutions/1_report_repair.py
+++ solutions/1_report_repair.py
@@ -7,3 +7,11 @@ for i in range(len(numbers)):
     for j in range(i, len(numbers)):
         if (numbers[i] + numbers[j]==2020):
             print(numbers[i]*numbers[j])
+
+# Part Two
+
+for i in range(len(numbers)):
+    for j in range(i, len(numbers)):
+        for k in range(j, len(numbers)):
+            if (numbers[i] + numbers[j] + numbers[k]==2020):
+                print(numbers[i]*numbers[j]*numbers[k])
