f1cdab4d2460b08793c7dc27d9706bbf04385dcb
GaboGomez09 <gomezgabriel09@gmail.com>
2020-12-01 23:36:22 -0600

    Second day, second part done

--- solutions/2_password_philosophy.py
+++ solutions/2_password_philosophy.py
@@ -18,3 +18,20 @@ for i in range(len(password)):
         sum += 1
 
 print(sum)
+
+# Part Two
+
+sum = 0
+for i in range(len(password)):
+    low_limit = int(policy_limits[i].split("-")[0])
+    high_limit = int(policy_limits[i].split("-")[1])
+    letter = policy_char[i][0]
+    pswrd = password[i]
+    
+    first_cond = pswrd[low_limit - 1] == letter
+    second_cond = pswrd[high_limit - 1] == letter
+    final_cond = first_cond + second_cond == 1
+    if final_cond:
+        sum += 1
+
+print(sum)
